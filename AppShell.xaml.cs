ffc318ef9a13db11ff02011f49eb7200827d67c0
Juan Quéz <juanquez.dev@gmail.com>
2026-05-14 20:51:12 -0500

    chore: ecosena initial structure

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EcosenaApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace EcosenaApp
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("Inter_18pt-Light.ttf", "InterLight");
+                    fonts.AddFont("Inter_18pt-Medium.ttf", "InterMedium");
+                    fonts.AddFont("Inter_18pt-Regular.ttf", "InterRegular");
+                    fonts.AddFont("Inter_18pt-SemiBold.ttf", "InterSemiBold");
+                    fonts.AddFont("Inter_18pt-Bold.ttf", "InterBold");
+                });
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}

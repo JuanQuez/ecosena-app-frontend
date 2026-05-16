676871b0f6986fce614528e31581f23cb6382eab
Juan Quéz <juanquez.dev@gmail.com>
2026-05-15 21:24:27 -0500

    feat: add splash screen

---
+++ Views/SplashPage.xaml.cs
@@ -0,0 +1,16 @@
+namespace EcosenaApp.Views;
+
+public partial class SplashPage : ContentPage
+{
+	public SplashPage()
+	{
+		InitializeComponent();
+	}
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        await Task.Delay(3000);
+        await Shell.Current.GoToAsync("//LoginPage");
+    }
+}

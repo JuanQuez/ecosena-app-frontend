18018263a9896add88855d369ed4d750f8caa481
Juan Quéz <juanquez.dev@gmail.com>
2026-05-16 22:50:51 -0500

    feat: add sign up page UI

--- Views/Auth/LoginPage.xaml.cs
+++ Views/Auth/LoginPage.xaml.cs
@@ -1,9 +1,15 @@
 namespace EcosenaApp.Views.Auth;
+using EcosenaApp.Views.Auth;
 
 public partial class LoginPage : ContentPage
 {
 	public LoginPage()
 	{
-		InitializeComponent();
+        InitializeComponent();
 	}
+
+    private async void OnSignUpTapped(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new SingUpPage());
+    }
 }
---
+++ Views/Auth/SingUpPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace EcosenaApp.Views.Auth;
+
+public partial class SingUpPage : ContentPage
+{
+	public SingUpPage()
+	{
+		InitializeComponent();
+	}
+}
